a4c7ba4e9ecb7cbfb26561ba8b1222e5c269a965
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2026-08-12 22:40:51 -0500

    chore: replace ESLint configuration file and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  { ignores: ["dist"] },
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  reactHooks.configs.flat.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    linterOptions: { reportUnusedDisableDirectives: "error" },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    settings: { react: { version: "19.2" } },
+    plugins: { "react-refresh": reactRefresh },
+    rules: {
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
